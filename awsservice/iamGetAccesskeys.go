75f23c3473d5d9552e43376dee501a3a66f40096
Pierre <pierre.poree@d2si.io>
2020-04-24 01:06:22 +0200

    add function to retrieve Accesskey last used date

--- awsservice/iamGetAccesskeys.go
+++ awsservice/iamGetAccesskeys.go
@@ -12,15 +12,17 @@ type AccessKey struct {
 	Id         string
 	CreateDate *time.Time
 	Status     string
+	LastUsed   *time.Time
 }
 
 func ListAccessKeysInfo(a []AccessKey) string {
 	for _, a := range a {
-		fmt.Printf("AccessKeyId: %v - CreateDate: %v - Status: %v\n", a.Id, a.CreateDate, a.Status)
+		fmt.Printf("AccessKeyId: %v - CreateDate: %v - Status: %v - LastUsed: %v\n", a.Id, a.CreateDate, a.Status, a.LastUsed)
 	}
 	return ""
 }
 
+// Func return bool if user has access keys
 func UserHasAccessKey(svc *iam.IAM, user *iam.UserDetail) bool {
 	input := &iam.ListAccessKeysInput{
 		UserName: aws.String(*user.UserName),
@@ -28,32 +30,46 @@ func UserHasAccessKey(svc *iam.IAM, user *iam.UserDetail) bool {
 
 	result, err := svc.ListAccessKeys(input)
 	if err != nil {
-		fmt.Sprintf("Error: %v", err)
+		fmt.Println("Error:", err)
 	}
 	for _, r := range result.AccessKeyMetadata {
 		if r.Status == aws.String("Active") {
 			return true
 		}
 	}
-	//fmt.Println(result)
 	return false
 }
 
+// Func to list accesskeys of a user
+// Return a slice of AccessKey struct
 func ListAccessKeys(svc *iam.IAM, user *string) (keys []AccessKey) {
 	input := &iam.ListAccessKeysInput{
 		UserName: aws.String(*user),
 	}
 	result, err := svc.ListAccessKeys(input)
 	if err != nil {
-		fmt.Sprintf("Error: %v", err)
+		fmt.Println("Error:", err)
 	}
 	for _, r := range result.AccessKeyMetadata {
 		keys = append(keys, AccessKey{
 			Id:         *r.AccessKeyId,
 			CreateDate: r.CreateDate,
 			Status:     *r.Status,
+			LastUsed:   getAccessKeyLastUsed(svc, *r.AccessKeyId),
 		})
 	}
-	//fmt.Println(keys)
 	return keys
+}
+
+// Func to retrieve the lastUsedDate of an AccessKey
+// Param: key string
+// return timestamp
+func getAccessKeyLastUsed(svc *iam.IAM, key string) *time.Time {
+	result, err := svc.GetAccessKeyLastUsed(&iam.GetAccessKeyLastUsedInput{
+		AccessKeyId: aws.String(key),
+	})
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	return result.AccessKeyLastUsed.LastUsedDate
 }
