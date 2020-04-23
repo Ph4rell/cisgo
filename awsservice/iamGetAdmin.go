e67168e50d4a0a821352124699bf060f6ba7188f
Pierre <pierre.poree@d2si.io>
2020-04-24 01:05:39 +0200

    add func to get group policy

--- awsservice/iamGetAdmin.go
+++ awsservice/iamGetAdmin.go
@@ -1,6 +1,11 @@
 package awsservice
 
-import "github.com/aws/aws-sdk-go/service/iam"
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/service/iam"
+)
 
 func IsUserAdmin(svc *iam.IAM, user *iam.UserDetail, admin string) bool {
 	// Check policy, attached policy, and groups (policy and attached policy)
@@ -14,10 +19,74 @@ func IsUserAdmin(svc *iam.IAM, user *iam.UserDetail, admin string) bool {
 		return true
 	}
 
-	// userGroupsHaveAdmin := UsersGroupsHaveAdmin(svc, user, admin)
-	// if userGroupsHaveAdmin {
-	// 	return true
-	// }
+	userGroupsHaveAdmin := UsersGroupsHaveAdmin(svc, user, admin)
+	if userGroupsHaveAdmin {
+		return true
+	}
+
+	return false
+}
+
+func UsersGroupsHaveAdmin(svc *iam.IAM, user *iam.UserDetail, admin string) bool {
+	input := &iam.ListGroupsForUserInput{UserName: user.UserName}
+	result, err := svc.ListGroupsForUser(input)
+	if err != nil {
+		fmt.Println("Got error getting groups for user:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	for _, group := range result.Groups {
+		groupPolicyHasAdmin := GroupPolicyHasAdmin(svc, group, admin)
+
+		if groupPolicyHasAdmin {
+			return true
+		}
+
+		attachedGroupPolicyHasAdmin := AttachedGroupPolicyHasAdmin(svc, group, admin)
+
+		if attachedGroupPolicyHasAdmin {
+			return true
+		}
+	}
+
+	return false
+}
+
+func GroupPolicyHasAdmin(svc *iam.IAM, group *iam.Group, admin string) bool {
+	input := &iam.ListGroupPoliciesInput{
+		GroupName: group.GroupName,
+	}
+
+	result, err := svc.ListGroupPolicies(input)
+	if err != nil {
+		fmt.Println("Got error calling ListGroupPolicies for group", group.GroupName)
+	}
+
+	// Wade through policies
+	for _, policyName := range result.PolicyNames {
+		if *policyName == admin {
+			return true
+		}
+	}
+
+	return false
+}
+
+func AttachedGroupPolicyHasAdmin(svc *iam.IAM, group *iam.Group, admin string) bool {
+	input := &iam.ListAttachedGroupPoliciesInput{GroupName: group.GroupName}
+	result, err := svc.ListAttachedGroupPolicies(input)
+	if err != nil {
+		fmt.Println("Got error getting attached group policies:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	for _, policy := range result.AttachedPolicies {
+		if *policy.PolicyName == admin {
+			return true
+		}
+	}
 
 	return false
 }
