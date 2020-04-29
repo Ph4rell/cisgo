311fe7daf772a589614408cf0b0b3f0fee1a8de6
Pierre <pierre.poree@d2si.io>
2020-04-29 14:40:45 +0200

    add function isUsed() and add it to the policy list

--- awsservice/iamGetPolicy.go
+++ awsservice/iamGetPolicy.go
@@ -9,11 +9,13 @@ import (
 
 type Policy struct {
 	Id              string
+	Name            string
 	AttachmentCount int
+	IsUsed          bool
 }
 
-// TODO add filter to input
-func ListPolicies(svc *iam.IAM) (policies []Policy) {
+// Func return a list of CustomerPolicy
+func ListCustomerPolicies(svc *iam.IAM) (policies []Policy) {
 	input := &iam.GetAccountAuthorizationDetailsInput{
 		Filter: []*string{
 			aws.String("LocalManagedPolicy"),
@@ -27,7 +29,14 @@ func ListPolicies(svc *iam.IAM) (policies []Policy) {
 		policies = append(policies, Policy{
 			Id:              *p.PolicyId,
 			AttachmentCount: int(*p.AttachmentCount),
+			Name:            *p.PolicyName,
+			IsUsed:          isUsed(*p.AttachmentCount),
 		})
 	}
 	return policies
+}
+
+// Func return true if AttachementCount is != zero
+func isUsed(c int64) bool {
+	return c != *aws.Int64(0)
 }
